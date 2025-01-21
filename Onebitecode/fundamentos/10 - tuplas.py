744e38847af32ab866ef34d2b1a66d3506264d86
MouraGabri <mouragabriel351@yahoo.com>
2025-01-20 21:04:32 -0300

    Estudei novamente e pratiquei novos exercícios

--- Onebitecode/fundamentos/10 - tuplas.py
+++ Onebitecode/fundamentos/10 - tuplas.py
@@ -1,13 +1,31 @@
-games_tuple = ("Mario","Zelda","GTA IV","Fortnite")
+nome_carros = []
 
-# 1 - Buscar os dois itens da tupla
-print(games_tuple[:2])
-
-# 2 - Buscar o último dado da tupla
-print(games_tuple[-1])
-
-# 3 - Buscar jogos até uma determinada posição
-print(games_tuple[:3])
-
-# 4 - Recuperar um item da tupla pelo indice
-print(games_tuple.index("Zelda"))
+# Cadastrando uma tupla de Caracteristícas de carro em uma lista
+def cadastrar_nomes_carro():
+    while True:
+        escolha = input("Deseja cadastrar um carro?").upper()
+        if escolha == 'SIM':
+            carro = input("Informe o nome do carro:").upper()
+            modelo = input("Informe o modelo do carro:").upper()
+            ano_carro = input("Informe o ano do carro:").upper()
+            nome_carros.append((carro,modelo,ano_carro))
+        else:
+            break    
+    print(f'Carros cadastrados:{nome_carros}', end= ' | ')
+print()
+def remover_carro():       
+    remover_veiculo = input("Deseja remover um carro?").upper()
+    if remover_veiculo == 'SIM':
+        carro_remover = input("Informe o nome do carro:").upper()
+        modelo = input("Informe o modelo do carro:").upper()
+        ano_carro = input("Informe o ano do carro:").upper()
+        for tupla in nome_carros:
+            if tupla[0] == carro_remover and tupla[1] == modelo and tupla[2] == ano_carro:
+                print('esta na lista')
+                tuplas_remover = (carro_remover,modelo,ano_carro)
+                nome_carros.remove(tuplas_remover)
+                print(f'Carros cadastrados:{nome_carros}', end= ' | ')
+            else:
+                print('Não possui cadastro')    
+cadastrar_nomes_carro()
+remover_carro()    
--- Onebitecode/fundamentos/8 - listas.py
+++ Onebitecode/fundamentos/8 - listas.py
@@ -1,12 +1,28 @@
+lista_produtos = []
 
 
-games_list = ['Fifa','Mario', 'GTA', 'Red Dead 2', 'Zelda']
+def cadastrando_produtos():
+    quantidade_produtos = int(input('Informe a quantidade de produtos para comprar:').strip())
+    produtos_comprados = 0
+    while produtos_comprados < quantidade_produtos:
+        produtos = input('Lista do supermercado:')
+        lista_produtos.append(produtos)
+        produtos_comprados +=1
+    print('------------------------------------')
 
-# 1 - Buscar os dois primeiros itens da lista
-# print(games_list[0:2])
-
-# 2 - Buscar último item da lista
-print(games_list[-1])
-
-# 3 - Buscar jogos até uma determinada posição
-print(games_list[:4])
+def remover_produtos():
+    print(f'Produtos: {lista_produtos}', end= ' | ')
+    print()
+    
+    while True:
+        remover = input('Deseja remover produtos?')
+        if remover == 'SIM':
+            nome_produtos_remover = input('Informe o nome do produto para remover:').strip()
+            lista_produtos.remove(nome_produtos_remover)
+            print(f'Produtos Na lista: {lista_produtos}', end= ' | ')    
+        else:
+            print('Encerrando..')
+            exit()
+                            
+cadastrando_produtos()
+remover_produtos()
